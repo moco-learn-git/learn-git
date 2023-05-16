01ddc2913cdd17b571b15bdea766ebeaf0f0a41b
Ben Coleman <colemanb@moravian.edu>
2023-05-16 16:03:25 -0400

    add / and correct references

--- app.py
+++ app.py
@@ -3,9 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route('/')
+def root():
+    return render_template('index.html')
+
+
 @app.route('/home')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
 
 @app.route('/step1')
