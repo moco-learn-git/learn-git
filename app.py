a0105d07f4abc14675daed76349c0a9611ff4d48
Ben Coleman <colemanb@moravian.edu>
2023-05-30 09:32:50 -0400

    Add Github login

--- app.py
+++ app.py
@@ -1,25 +1,77 @@
-from flask import Flask, render_template
+from flask import Flask, url_for, session
+from flask import render_template, redirect
+from authlib.integrations.flask_client import OAuth
+import requests
+import dotenv
+import os
+
+
+dotenv.load_dotenv()
+GITHUB_CLIENT_ID = os.getenv('GITHUB_CLIENT_ID')
+GITHUB_CLIENT_SECRET = os.getenv('GITHUB_CLIENT_SECRET')
+APP_SECRET = os.getenv('APP_SECRET')
 
 app = Flask(__name__)
+app.secret_key = APP_SECRET
+oauth = OAuth(app)
+
+oauth.register(
+    name='github',
+    client_id=GITHUB_CLIENT_ID,
+    client_secret=GITHUB_CLIENT_SECRET,
+    access_token_url='https://github.com/login/oauth/access_token',
+    access_token_params=None,
+    authorize_url='https://github.com/login/oauth/authorize',
+    authorize_params=None,
+    api_base_url='https://api.github.com/',
+    client_kwargs={'scope': 'user:email'},
+)
 
 
 @app.route('/')
 def root():
-    return render_template('index.html')
+    user = session.get('user')
+    return render_template('index.html', user=user)
+
+
+@app.route('/login')
+def login():
+    redirect_uri = url_for('auth', _external=True)
+    return oauth.github.authorize_redirect(redirect_uri)
+
+
+@app.route('/authorize')
+def auth():
+    token = oauth.github.authorize_access_token()
+    print(token)
+    bearer = token['access_token']
+    headers = {"Authorization": "Bearer {}".format(bearer)}
+    data = requests.get('https://api.github.com/user', headers=headers).json()
+    session['user'] = data['login']
+    return redirect('/')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect('/')
 
 
 @app.route('/home')
 def home():
+    user = session.get('user')
     return render_template('home.html')
 
 
 @app.route('/step1')
 def step1():
+    user = session.get('user')
     return render_template('step1.html')
 
 
 @app.route('/step2')
 def step2():
+    user = session.get('user')
     return render_template('step2.html')
 
 
